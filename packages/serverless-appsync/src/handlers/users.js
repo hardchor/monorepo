80c79a11a73d38fc743a8707c73d0a91781bbb4f
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-03-03 23:49:21 +0100

    create db entry on user signup

--- packages/serverless-appsync/src/handlers/users.js
+++ packages/serverless-appsync/src/handlers/users.js
@@ -1,5 +1,26 @@
-export function create(event, context, callback) {
+import { DynamoDB } from 'aws-sdk';
+import config from '../config';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+export async function create(event, context, callback) {
   console.log('##### create', event, context);
+
+  const putData = await dynamoDb
+    .put({
+      TableName: config.userTable,
+      Item: { handle: event.userName },
+    })
+    .promise();
+  console.log('##### put data', putData);
+
+  const data = await dynamoDb
+    .get({
+      TableName: config.userTable,
+      Key: { handle: event.userName },
+    })
+    .promise();
+  console.log('##### get data', data);
 
   // create entry in users table
 
