a3c7383f10b1b76ed8910bcdb90e245e550da0a4
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-03-05 20:31:48 +0100

    set appsync urk

---
+++ packages/nextjs/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  appSyncId: 'hjbh2z4wpve5xfk76yp46xtjl4',
+  appSyncUrl: 'https://n43nzhde5ffufphgildam3lqdq.appsync-api.eu-west-1.amazonaws.com/graphql',
+};
--- packages/nextjs/pages/index.js
+++ packages/nextjs/pages/index.js
@@ -1,5 +1,15 @@
+import Head from 'next/head';
+
 function Home() {
-  return <div>Welcome to next.js!</div>;
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
 }
 
 export default Home;
