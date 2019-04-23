d577b94064263f4159e94091afb3e0d98e89b693
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-04-23 23:23:05 +0200

    add analytics, file upload

--- packages/amplify-react/src/App.tsx
+++ packages/amplify-react/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component, ChangeEvent } from 'react';
+import Amplify, { Analytics, Storage } from 'aws-amplify';
+import { withAuthenticator, S3Album } from 'aws-amplify-react';
 import logo from './logo.svg';
+import awsmobile from './aws-exports';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsmobile);
+
+Storage.configure({ level: 'private' });
+
+class App extends Component {
+  uploadFile = (evt: ChangeEvent<HTMLInputElement>) => {
+    if (!evt.target.files) return;
+
+    const file = evt.target.files[0];
+    const name = file.name;
+
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name });
+    });
+  };
+
+  componentDidMount() {
+    Analytics.record('Amplify_CLI');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <S3Album level="private" path="" />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, true);
