1372972ff8d20e825551ce2a6cc44b55b6f94df6
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-04-23 23:55:35 +0200

    add graphql, dynamodb

--- packages/amplify-react/src/App.tsx
+++ packages/amplify-react/src/App.tsx
@@ -1,13 +1,33 @@
 import React, { Component, ChangeEvent } from 'react';
-import Amplify, { Analytics, Storage } from 'aws-amplify';
+import Amplify, { Analytics, Storage, API, graphqlOperation } from 'aws-amplify';
 import { withAuthenticator, S3Album } from 'aws-amplify-react';
-import logo from './logo.svg';
 import awsmobile from './aws-exports';
 import './App.css';
 
 Amplify.configure(awsmobile);
 
 Storage.configure({ level: 'private' });
+
+const listTodos = `query listTodos {
+  listTodos {
+    items {
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name: String! $description: String!) {
+  createTodo(input: {
+    name: $name
+    description: $description
+  }) {
+    id
+    name
+    description
+  }
+}`;
 
 class App extends Component {
   uploadFile = (evt: ChangeEvent<HTMLInputElement>) => {
@@ -21,6 +41,22 @@ class App extends Component {
     });
   };
 
+  todoMutation = async () => {
+    const todoDetails = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!',
+    };
+
+    const newEvent = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newEvent));
+  };
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  };
+
   componentDidMount() {
     Analytics.record('Amplify_CLI');
   }
@@ -30,6 +66,10 @@ class App extends Component {
       <div className="App">
         <p> Pick a file</p>
         <input type="file" onChange={this.uploadFile} />
+
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+
         <S3Album level="private" path="" />
       </div>
     );
