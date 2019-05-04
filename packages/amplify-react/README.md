58ff03388b53edc67b5e1c4a31d604e262097865
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-05-04 14:11:20 +0200

    update form

--- packages/amplify-react/README.md
+++ packages/amplify-react/README.md
@@ -4,9 +4,9 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
-### `yarn start`
+### `yarn serve`
 
-Runs the app in the development mode.<br>
+Deploys infrastructure and runs the app in the development mode.<br>
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 The page will reload if you make edits.<br>
@@ -17,9 +17,9 @@ You will also see any lint errors in the console.
 Launches the test runner in the interactive watch mode.<br>
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `yarn build`
+### `yarn deploy`
 
-Builds the app for production to the `build` folder.<br>
+Builds the app for production to the `build` folder and deploys to production.<br>
 It correctly bundles React in production mode and optimizes the build for the best performance.
 
 The build is minified and the filenames include the hashes.<br>
--- packages/amplify-react/src/Components/Participant/index.tsx
+++ packages/amplify-react/src/Components/Participant/index.tsx
@@ -1,26 +1,45 @@
-import React, { FormEvent, FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
+import { useForm, useField } from 'react-final-form-hooks';
+import { Config } from 'final-form';
+import Joi from '@hapi/joi';
 import { createParticipant } from '../../graphql/mutations';
-import { CreateParticipantMutationVariables } from '../../API';
+import { CreateParticipantMutationVariables, CreateParticipantInput } from '../../API';
+import validateForm from '../../validation/validateForm';
 
 const createParticipantMutation = (data: CreateParticipantMutationVariables) =>
   graphqlOperation(createParticipant, data);
 
-const ParticipantForm: FunctionComponent = () => {
-  const [name, setName] = useState('');
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+const schema = Joi.object().keys({
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+});
+const validate = validateForm(schema);
 
-    await API.graphql(createParticipantMutation({ input: { name } }));
+const ParticipantForm: FunctionComponent = () => {
+  const onSubmit = async (input: CreateParticipantInput) => {
+    await API.graphql(createParticipantMutation({ input }));
   };
+
+  const { form, handleSubmit, pristine, submitting } = useForm<Config<CreateParticipantInput>>({
+    onSubmit,
+    validate,
+  });
+  const name = useField('name', form);
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        What's your name?
-        <input type="text" name="name" value={name} onChange={e => setName(e.target.value)} />
-      </label>
-      <input type="submit" value="Submit" />
+      <div>
+        <label>Your name</label>
+        <input {...name.input} placeholder="Name" />
+        {name.meta.touched && name.meta.error && <span>{name.meta.error}</span>}
+      </div>
+      <button type="submit" disabled={pristine || submitting}>
+        Submit
+      </button>
     </form>
   );
 };
