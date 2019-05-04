312d53560aba2bce7e68834eb0cd2c027bad9c22
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-05-04 17:24:45 +0200

    add google auth

--- packages/amplify-react/src/Components/App.tsx
+++ packages/amplify-react/src/Components/App.tsx
@@ -1,22 +1,27 @@
 import React, { FunctionComponent, useEffect } from 'react';
-import Amplify, { Analytics } from 'aws-amplify';
+import Amplify, { Analytics, Auth } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
-import awsmobile from '../aws-exports';
+import awsconfig from '../aws-exports';
 import './App.css';
-import Participant from './Participant';
+// import Participant from './Participant';
 
-Amplify.configure(awsmobile);
+Amplify.configure(awsconfig);
 
 const App: FunctionComponent = () => {
   useEffect(() => {
-    Analytics.record('Amplify_CLI');
+    // Analytics.record('Amplify_CLI');
   }, []);
 
   return (
     <div className="App">
-      <Participant />
+      Hi!
+      {/* <button onClick={() => Auth.federatedSignIn()}>Sign In</button> */}
+      {/* <Participant /> */}
     </div>
   );
 };
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, true, [], {
+  google_client_id: '1003625021989-0f9t1b02kh6ss8fonvam84pk9eierbcv.apps.googleusercontent.com',
+});
+// export default App;
