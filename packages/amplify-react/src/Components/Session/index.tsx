649f42863b8178014246e6619b35675e8e67bfdb
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-05-10 01:54:24 +0200

    retrieve session from gql

--- packages/amplify-react/src/Components/Session/index.tsx
+++ packages/amplify-react/src/Components/Session/index.tsx
@@ -1,7 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { GetSessionQueryVariables } from '../../API';
+import { getSession as getSessionQuery } from '../../graphql/queries';
+import { graphqlOperation, API } from 'aws-amplify';
 
-const Session: FunctionComponent<RouteComponentProps> = () => {
+const getSession = (query: GetSessionQueryVariables) => graphqlOperation(getSessionQuery, query);
+
+const Session: FunctionComponent<RouteComponentProps<{ sessionId: string }>> = ({ sessionId }) => {
+  const [session, setSession] = useState({});
+
+  useEffect(() => {
+    const fetchSession = async (id: string) => {
+      const result = await API.graphql(getSession({ id }));
+      setSession(result);
+    };
+
+    if (!sessionId) return;
+
+    fetchSession(sessionId);
+  }, [sessionId]);
+  console.log('#####', session);
+
   return (
     <div>
       <h2>Session</h2>
