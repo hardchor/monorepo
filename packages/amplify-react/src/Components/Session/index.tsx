627902d56292fff80af2a6072a2183166a9e1820
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-05-12 13:52:40 +0200

    display session

--- packages/amplify-react/src/Components/Session/index.tsx
+++ packages/amplify-react/src/Components/Session/index.tsx
@@ -1,29 +1,40 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { GetSessionQueryVariables } from '../../API';
+import { GetSessionQueryVariables, GetSessionQuery } from '../../API';
 import { getSession as getSessionQuery } from '../../graphql/queries';
 import { graphqlOperation, API } from 'aws-amplify';
+import { GraphQLResult } from '@aws-amplify/api/lib/types';
+
+declare type GetSessionResult = GraphQLResult & {
+  data: GetSessionQuery;
+};
+declare type Session = {
+  id: string;
+  name: string | null;
+};
 
 const getSession = (query: GetSessionQueryVariables) => graphqlOperation(getSessionQuery, query);
 
 const Session: FunctionComponent<RouteComponentProps<{ sessionId: string }>> = ({ sessionId }) => {
-  const [session, setSession] = useState({});
+  const [session, setSession] = useState({} as Session);
+
+  const fetchSession = async (id: string) => {
+    const { data } = (await API.graphql(getSession({ id }))) as GetSessionResult;
+
+    if (data.getSession) {
+      setSession(data.getSession);
+    }
+  };
 
   useEffect(() => {
-    const fetchSession = async (id: string) => {
-      const result = await API.graphql(getSession({ id }));
-      setSession(result);
-    };
-
     if (!sessionId) return;
 
     fetchSession(sessionId);
   }, [sessionId]);
-  console.log('#####', session);
 
   return (
     <div>
-      <h2>Session</h2>
+      <h2>Session {session.id}</h2>
     </div>
   );
 };
